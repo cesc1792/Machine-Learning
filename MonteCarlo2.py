58da20cb3fd83c310e0e5a795fffba843af51735
cesc1792 <tranbaophi92@gmail.com>
2018-04-21 13:21:47 +0200

    Add files via upload
    with visualization

---
+++ MonteCarlo2.py
@@ -0,0 +1,61 @@
+
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+
+# let us go ahead and change this to return a simple win/loss
+def rollDice():
+    roll = random.randint(1,100)
+
+    if roll == 100:
+        #print roll,'roll was 100, you lose. What are the odds?! Play again!'
+        return False
+    elif roll <= 50:
+        #print roll,'roll was 1-50, you lose.'
+        return False
+    elif 100 > roll >= 50:
+        #print roll,'roll was 51-99, you win! *pretty lights flash* (play more!)'
+        return True
+
+
+'''
+Simple bettor, betting the same amount each time.
+'''
+def simple_bettor(funds,initial_wager,wager_count):
+    value = funds
+    wager = initial_wager
+
+    wX=[]
+    vY=[]
+
+    currentWager = 1
+
+    while currentWager <= wager_count:
+        if rollDice():
+            value += wager
+            wX.append(currentWager)
+            vY.append(value)
+        else:
+            value -= wager
+            wX.append(currentWager)
+            vY.append(value)
+
+        currentWager += 1
+
+    # changed to reduce spam
+    #if value < 0:
+        #value = 'Broke!'
+    #print ('Funds:', value)
+    plt.plot(wX,vY)
+# lots of wagers now....
+x = 0
+
+while x < 100:
+    simple_bettor(10000,100,500)
+    x += 1
+
+plt.ylabel('Account Value')
+plt.xlabel('Wager Count')
+plt.show()
